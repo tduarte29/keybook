f27220df42b32196af3bb2e8ad27101f7c57679d
tduarte29 <129213483+tduarte29@users.noreply.github.com>
2025-06-25 14:46:40 -0300

    update

--- src/main/java/com/andrevsc/keybook/KeybookApplication.java
+++ src/main/java/com/andrevsc/keybook/KeybookApplication.java
@@ -2,12 +2,16 @@ package com.andrevsc.keybook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan; // adicione este import
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories; // adicione este import
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.andrevsc.keybook.model") // adicione esta linha
+@EnableJpaRepositories(basePackages = "com.andrevsc.keybook.repository") // adicione esta linha
 public class KeybookApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KeybookApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KeybookApplication.class, args);
+    }
 
 }
